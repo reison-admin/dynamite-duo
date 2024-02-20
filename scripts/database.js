5823f9e4e42db2e972c970723c236ea55e5b12e8
Jeff Ewers <89662718+reison-admin@users.noreply.github.com>
2024-02-20 14:33:54 -0600

    Setup modules

---
+++ scripts/database.js
@@ -0,0 +1,34 @@
+const database = {
+    heroes: [
+        {
+            id: 1,
+            name: "Dynoguy",
+            power: "Fire blasts"
+        },
+        {
+            id: 2,
+            name: "Hyperspeed",
+            power: "Super speed"
+        }
+    ],
+    villains: [
+        {
+            id: 1,
+            name: "Meteorman",
+            power: "Meteorite impacts"
+        },
+        {
+            id: 2,
+            name: "Brownleaf",
+            power: "Decay"
+        }
+    ]
+}
+
+export const getHeroes = () => {
+    return database.heroes.map(hero => ({...hero}))
+}
+
+export const getVillains = () => {
+    return database.villains.map(villain => ({...villain}))
+}
